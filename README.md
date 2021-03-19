23826867a59462c73917c93b1331b1138592d2f3
Samson Yu Bai Jian <36767958+SamsonYuBaiJian@users.noreply.github.com>
2021-03-19 11:46:20 +0800

    Add stat saving

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CLEVEREST
 
 # TODOs
+- [X] Data processing
 - [X] Build basic model
 - [X] Add coordinate conditioning for object identification/tracking
 - [ ] Add MSE loss for generated images
--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def main(task, num_epoch, batch_size):
 
     model.train()
     print('Training...')
-    # train_loss_per_epoch = []
+    train_loss_per_epoch = []
     for i in range(num_epoch):
         temp_train_loss = []
         for j, batch in enumerate(train_dataloader):
             frames, coordinates, labels = batch
-            coordinates = torch.stack(coordinates)
+            coordinates = torch.stack(coordinates).T
             preds = model(task, coordinates, frames)
             labels = torch.unsqueeze(labels, dim=1).type_as(preds)
             loss = bce_logits_loss(preds, labels) # + mse_loss()
@@ -40,14 +40,17 @@ def main(task, num_epoch, batch_size):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            print("Epoch {} batch {}/{} training done.".format(i+1, j+1, len(train_dataloader)))
-        
+            print("Epoch {} batch {}/{} training done with average loss = {}.".format(i+1, j+1, len(train_dataloader), sum(temp_train_loss) / len(temp_train_loss)))
+
         print("Epoch {} train loss =".format(i+1), sum(temp_train_loss) / len(temp_train_loss))
-        # train_loss_per_epoch.append(sum(temp_train_loss) / len(temp_train_loss))
+        train_loss_per_epoch.append(sum(temp_train_loss) / len(temp_train_loss))
+
+    with open('loss.txt', 'w') as f:
+        f.write(str(train_loss_per_epoch))
 
 
 if __name__ == '__main__':
     num_epoch = 10
-    batch_size = 4
+    batch_size = 6
 
     main('contact', num_epoch, batch_size)
